7554dba12293ffe47115a36094898ff90445ac59
Pablo Trinidad <pabloatp0204@gmail.com>
2016-08-21 20:23:04 -0500

    Sponsors admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sponsor
+
+
+@admin.register(Sponsor)
+class SponsorAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id', 
+        'first_name', 
+        'last_name', 
+        'email', 
+        'company',
+        'message',
+    )
+
+    search_fields = (
+        'first_name', 
+        'last_name', 
+        'email', 
+        'company',
+    )
+
+    list_filter = (
+        'created',
+        'modified',
+    )
